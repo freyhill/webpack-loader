52d6cd5f27e7b8dd65cdae32541b020a40bb1f7b
leinov <1425795233@qq.com>
2016-04-05 12:14:01 +0800

    add loaders

--- loader-example1/js/index.js
+++ loader-example1/js/index.js
@@ -4,6 +4,9 @@ require("../sass/style.scss");
 //获取json数据
 var datajson = require("./data.json");
 
+//获取fastclick
+var FastClick = require('fastclick');
+
 //获取模版
 var template = require('../tpl/tpl.html');
 
@@ -15,5 +18,14 @@ document.querySelector("#content").innerHTML = html;
 
 //使用es6
 import Obj from './obj.js';
- 
+
+//获取zepoto
+import $ from 'webpack-zepto';
+
+$(function(){
+  FastClick.attach(document.body);
+  $(".pic").click(function(){
+  	alert(0);
+  })
+})
 console.log(Obj.name);
